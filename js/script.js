14f133c777cba46d37273a3c730a9d043f1ae18e
Alejandro Perez <APerez89@fullsail.edu>
2016-03-01 01:42:58 -0500

    everything added. html icons background img
    html css icons background pics and more

---
+++ js/script.js
@@ -0,0 +1,22 @@
+// var sn = $(".nav-wrapper");
+
+// $(window).scroll(function() {
+// 	if( $(this).scrollTop() > 800 ) {
+// 		sn.addClass(".nav-wrapper-scrolled");
+// 	}
+// 	else {
+// 		sn.removeClass(".nav-wrapper-scrolled");
+// 	}
+// });
+
+// $(document).ready(function() {
+// 	$(window).bind('scroll', function() {
+// 	var navHeight = $(window).height() - 50;
+// 		if ($(window).scrollTop() > navHeight) {
+// 			$('.nav-wrapper').addClass('fixed');
+// 		}
+// 		else {
+// 			$('.nav-wrapper').removeClass('fixed');
+// 		}
+// 	});
+// });
